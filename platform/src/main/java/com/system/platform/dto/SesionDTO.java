e824fb487b9facff4f1fdd6bb5c6e1da9e80df85
ezequielfina <finaezeq@gmail.com>
2026-09-01 18:37:56 -0300

    sesion flujo

---
+++ platform/src/main/java/com/system/platform/dto/SesionDTO.java
@@ -0,0 +1,49 @@
+package com.system.platform.dto;
+
+import com.system.platform.entities.operaciones.EstadoSesion;
+import com.system.platform.entities.operaciones.Sesion;
+import jakarta.annotation.Nullable;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class SesionDTO {
+    public record Base (
+            UUID id
+    ) {}
+
+    public record Create (
+            ClaseDTO.Base clase,
+            EspacioDTO.Base espacio,
+            LocalDate fecha,
+            EstadoSesion estadoSesion
+    ) {}
+
+    public record Update (
+            @Nullable ClaseDTO.Base clase,
+            @Nullable EspacioDTO.Base espacio,
+            @Nullable Date fecha,
+            @Nullable EstadoSesion estadoSesion
+    ) {}
+
+    public record Response (
+            UUID id,
+            ClaseDTO.Response clase,
+            EspacioDTO.Response espacio,
+            LocalDate fecha,
+            EstadoSesion estadoSesion
+    ) {
+        public static Response fromEntity(Sesion data) {
+            return new Response(
+                    data.getId(),
+                    ClaseDTO.Response.fromEntity(data.getClase()),
+                    EspacioDTO.Response.fromEntity(data.getEspacio()),
+                    data.getFecha(),
+                    data.getEstado()
+            );
+        }
+    }
+}
